1b59c99dc86fbaf910a1f76daf4afae6b60e37c2
Hannah D. Annor-Blay <hannahadonkor@gmail.com>
2023-02-07 21:38:38 +0000

    styled the buttons

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,12 +1,15 @@
 <template>
-  <div> <h1>Color Picker Game</h1>
+  <div class="main">
+    <h1>Color Picker Game</h1>
   
-    <div>{{ message }}</div>
+    <div class="value" >{{ message }}</div>
   
     <button v-for="color in colors" :key="color" @click="matchColor(color)">
       {{ color }}
-    </button></div> 
-  </template>
+    </button>
+  </div> 
+
+</template>
   
   <script setup>
 
